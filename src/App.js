2f86c7f2777236e46517d63817116e6c9ca3b3d2
Sheub <sebastien@sheub.eu>
2019-03-30 15:56:37 +0100

    add all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import $ from 'jquery';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+// import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
+
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    // ReactGA.initialize('UA-110570651-1');
+    // ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +61,16 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        
+        <Header data={this.state.resumeData.main}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
+      
       </div>
     );
   }
